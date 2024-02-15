d088102273d26280b74ad47e9213c2b598b0fc4d
Manikanta Magaji <manikanta.magaji@gmail.com>
2024-02-15 07:42:07 +0530

    init commit

---
+++ Todo.cs
@@ -0,0 +1,5 @@
+public class Todo {
+    public int Id { set; get; };
+    public string? Name{ set; get; };
+    public bool IsComplete{ set; get; };
+}
---
+++ TodoDb.cs
@@ -0,0 +1,5 @@
+class TodoDb : DbContext {
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+    public DbSet<Todo> Todos => sbyte<Todo>();
+}
